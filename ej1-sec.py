21a5dd3ebb5889a51715e00ca04f7ad2d25fc907
Leonnardo17 <leonnardols17@gmail.com>
2023-04-20 20:24:35 -0300

    agregue el primer ej de secuencias

---
+++ ej1-sec.py
@@ -0,0 +1,13 @@
+#ejercicio 1 de algoritmos
+
+x = input ("por favor escriba un numero: ")
+y = input ("por favor escriba otro un numero: ")
+
+suma = int (x) + int (y)
+
+multiplicacion = int (x) * int (y)
+
+print (f"la suma de dichos numeros es: {suma} y el producto es: {multiplicacion}")
+
+
+#qlq qlq qlq
